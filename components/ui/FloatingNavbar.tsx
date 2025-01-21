afbc4695bf3b706689868e9ccfc8e780af256446
Brett Schlesinger <brett.schlesinger@accenture.com>
2025-01-21 16:40:02 -0500

    slim nav on mobile

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -55,8 +55,10 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-4 py-2 items-center justify-center space-x-4",
-          className
+          "flex max-w-fit fixed top-5 md:top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-4 py-2 items-center justify-center space-x-4",
+          className,
+          "md:px-4 md:py-2 md:space-x-4",
+          "px-2 py-1 space-x-2"
         )}
       >
         {navItems.map(
@@ -65,7 +67,9 @@ export const FloatingNav = ({
               key={`link=${idx}`}
               href={navItem.link}
               className={cn(
-                "items-center flex space-x-1 dark:hover:text-neutral-300 hover:text-neutral-500 text-sm font-medium relative text-black dark:text-white px-2 py-2 rounded-full"
+                "items-center flex space-x-1 dark:hover:text-neutral-300 hover:text-neutral-500 text-sm font-medium relative text-black dark:text-white px-2 py-2 rounded-full",
+                "md:px-2 md:py-2",
+                "px-1 py-1"
               )}
             >
               <span className="text-sm">{navItem.name}</span>
